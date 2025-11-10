72a6a4d75b518a0567bdf54d651b7203d630a147
aralim11 <aralim11@gmail.com>
2025-11-10 11:19:50 +0600

    update cover

--- resources/views/frontEnd/portfolio/about.blade.php
+++ resources/views/frontEnd/portfolio/about.blade.php
@@ -4,21 +4,21 @@
         <div class="row">
             <div class="col-xl-6 image-block" data-aos="fade-right" data-aos-delay="200" data-aos-duration="2000">
                 <div class="img-wrapper about-img-wrap" data-tilt data-tilt-max="10">
-                    <img class="about-img-1 img-fluid" src="{{ asset('frontEnd/portfolio/image/image.png') }}"
-                        alt="about image">
+                    <img class="about-img-1 img-fluid" src="{{ asset('frontEnd/portfolio/image/IMG_E8007.png') }}"
+                        style="height: 634px; width: 508px;" alt="about image">
                 </div>
             </div>
             <div class="col-xl-6 content-block" data-aos="fade-right" data-aos-delay="400" data-aos-duration="2000">
-                <h2><span>About Me</span>{{$data['designation']}}</h2>
+                <h2><span>About Me</span>{{ $data['designation'] }}</h2>
                 <p>
-                    {{$data['description']}}
+                    {{ $data['description'] }}
                 </p>
                 <div class="personal-details row">
                     <div class="col-md-4">
                         <ul class="personal-info">
                             <li>
                                 <h4>Name</h4>
-                                <p>{{$data['full_name']}}</p>
+                                <p>{{ $data['full_name'] }}</p>
                             </li>
                         </ul> <!-- .personal-info -->
                     </div> <!-- .col-md-6 -->
@@ -26,7 +26,7 @@
                         <ul class="personal-info">
                             <li>
                                 <h4>Email</h4>
-                                <p>{{$data['email']}}</p>
+                                <p>{{ $data['email'] }}</p>
                             </li>
                         </ul> <!-- .personal-info -->
                     </div> <!-- .col-md-6 -->
@@ -34,7 +34,7 @@
                         <ul class="personal-info">
                             <li>
                                 <h4>Address</h4>
-                                <p>{{$data['address']}}</p>
+                                <p>{{ $data['address'] }}</p>
                             </li>
                         </ul> <!-- .personal-info -->
                     </div> <!-- .col-md-6 -->
